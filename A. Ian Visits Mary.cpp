3ea01f1263d29700c52d1895a73b66c86975e4d5
Mosabbir512 <107846555+Mosabbir512@users.noreply.github.com>
2024-05-31 08:02:40 +0600

    Add files via upload

---
+++ A. Ian Visits Mary.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define srt(x) sort(x.begin(),x.end())
+#define pb(x) push_back(x)
+#define print(x) for(auto it: x)cout<<it<<" ";cout<<endl
+#define ll long long
+#define endl '\n'
+void solve() {
+    int a,b;
+    cin>>a>>b;
+    if(abs(a-b)==1) {
+        cout<<1<<endl;
+        cout<<a<<" "<<b<<endl;
+        return;
+    }
+    if(a!=1e9) {
+        if(a>b) {
+            cout<<2<<endl;
+            cout<<a+1<<" "<<1<<endl;
+            cout<<a<<" "<<b<<endl;
+        }
+        else {
+            cout<<2<<endl;
+            cout<<1<<" "<<b+1<<endl;
+            cout<<a<<" "<<b<<endl;
+        }
+    } else {
+        cout<<2<<endl;
+        cout<<a-1<<" "<<b<<endl;
+        cout<<a<<" "<<b<<endl;
+    }
+
+}
+int main() {
+    fast;
+    int t=1;
+    cin>>t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
+
