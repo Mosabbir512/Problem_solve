b1efbf905facfdb71e068dd561f2d1871731f396
Mosabbir512 <107846555+Mosabbir512@users.noreply.github.com>
2024-06-05 06:55:27 +0600

    Add files via upload

---
+++ A. Add and Divide.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define srt(x) sort(x.begin(),x.end())
+#define pb(x) push_back(x)
+#define print(x) for(auto it: x)cout<<it<<" ";cout<<endl
+#define ll long long
+#define endl '\n'
+void solve() {
+    int aa,b,ct=0;
+    int ans=INT_MAX;
+    cin>>aa>>b;
+    if(b==1)b++,ct=1;
+    int tm=ct;
+    for(int i=b; i<=b+33; i++) {
+        ct=tm;
+        ct+=(i-b);
+        int a=aa;
+        while(a>=i) {
+            ct++;
+            a/=i;
+        }
+        if(a!=0)ct++;
+        ans=min(ans,ct);
+    }
+    cout<<ans<<endl;
+
+}
+int main() {
+    fast;
+    int t=1;
+    cin>>t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
+
+
