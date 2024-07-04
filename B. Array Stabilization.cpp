11809bc0ad201450a3341874d6a60d962bc43098
Mosabbir512 <107846555+Mosabbir512@users.noreply.github.com>
2024-07-04 23:47:28 +0600

    Add files via upload

---
+++ B. Array Stabilization.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+//#define srt(x) sort(x.begin(),x.end())
+#define pb(x) push_back(x)
+#define print(x) for(auto it: x)cout<<it<<" ";cout<<endl
+#define ll long long
+#define endl '\n'
+void solve(){
+     int n;cin>>n;
+     int arr[n];
+     for(int i=0;i<n;i++)cin>>arr[i];
+     sort(arr,arr+n);
+     int mx=INT_MIN,mn=INT_MAX;
+     for(int i=1;i<n;i++){
+        mx=max(mx,arr[i]);
+        mn=min(mn,arr[i]);
+     }
+     int ans=mx-mn;
+     mx=INT_MIN,mn=INT_MAX;
+     for(int i=0;i<n-1;i++){
+        mx=max(mx,arr[i]);
+        mn=min(mn,arr[i]);
+     }
+     ans=min(ans,mx-mn);
+     cout<<ans<<endl;
+
+}
+int main(){
+    fast;
+    int t=1;
+    //cin>>t;
+    while(t--){
+       solve();
+    }
+    return 0;
+}
+
+
