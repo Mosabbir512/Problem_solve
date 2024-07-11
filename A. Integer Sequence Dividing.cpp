7c6c43033376f898629a4532efa937c392b5e61e
Mosabbir512 <107846555+Mosabbir512@users.noreply.github.com>
2024-07-11 07:58:59 +0600

    Add files via upload

---
+++ A. Integer Sequence Dividing.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define srt(x) sort(x.begin(),x.end())
+#define pb(x) push_back(x)
+#define print(x) for(auto it: x)cout<<it<<" ";cout<<endl
+#define ll long long
+#define endl '\n'
+void solve(){
+     int n;cin>>n;
+     n=n%4;
+     if(n==1 || n==2)cout<<1<<endl;
+     else cout<<0<<endl;
+}
+int main(){
+    fast;
+    int t=1;
+    //cin>>t;
+    while(t--){
+       solve();
+    }
+    return 0;
+}
+
+
